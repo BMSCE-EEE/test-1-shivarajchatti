ba02e4dccae5b7a16b99aaf82f958665cab6b883
Shivaraj Chatti <95394116+shivarajchatti@users.noreply.github.com>
2021-12-04 03:04:26 +0000

    nil

--- p0final.c
+++ p0final.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
-int add(int a, int b)
+int add(int a,int b)
 {int c;
 c=a+b;
+return c;
 }
 int main()
 {int a,b,sum;
@@ -9,4 +10,5 @@ printf("enter two integers:");
 scanf("%d%d",&a,&b);
 sum=add(a,b);
 printf("sum of %d and %d is:%d",a,b,sum);
+return 0;
 }
